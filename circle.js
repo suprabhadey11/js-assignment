d002813547dd3cf0b06dc2cb17ab4154bc160d85
suprabhadey11 <93858236+suprabhadey11@users.noreply.github.com>
2021-12-11 23:41:23 +0530

    Add files via upload

---
+++ circle.js
@@ -0,0 +1,6 @@
+const Shape = require('./shape.js');
+class Circle extends Shape{
+calculateArea(){
+ console.log('hello');
+}
+}
---
+++ shape.js
@@ -0,0 +1,13 @@
+
+class Shape{
+ constructor(color){
+  this.color = color;
+ }
+ drawShape(){
+  console.log("Drawing Shape");
+ }
+ calculateArea(){
+  console.log("Calculating area of your shape")
+ }
+}
+module.exports = Shape;
